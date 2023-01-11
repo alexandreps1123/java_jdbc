726b13a5cfe683c02182e152c1b90f68ad4399fa
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-11 10:51:05 -0300

    update base salary seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,9 @@ public class Program {
 
         // insertSeller();
 
-        insertDepartment();
+        // insertDepartment();
+
+        updateBaseSalary();
     }
 
     public static void selectAndPrintDepartment() {
@@ -113,6 +115,36 @@ public class Program {
             else {
                 System.out.println("No rows afected!");
             }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+
+    public static void updateBaseSalary() {
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                "UPDATE seller "
+                + "SET BaseSalary = BaseSalary + ? "
+                + "WHERE "
+                + "(DepartmentId = ?)",
+                Statement.RETURN_GENERATED_KEYS);
+
+                st.setDouble(1, 200.0);
+                st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
         }
         catch (SQLException e) {
             e.printStackTrace();
